01bad3f7e978cc94496a660e76f6ca6de18af02f
Acedev003 <acedeveloper003@gmail.com>
2024-08-10 20:24:13 +0530

    Create Mock Object Factory

--- 2D/game.py
+++ 2D/game.py
@@ -1,9 +1,154 @@
 import pymunk
+import pygame
+import random
+from utils import generate_noise , pairwise
+
+class Categories:
+    LANDER_CAT  = 0b01
+    TERRAIN_CAT = 0b10
 
 class Lander:
-    def __init__(self,position):
+    def __init__(self,position,screen,space,mass,id=None):
+        
+        self.id     = id
+        self.screen = screen
+        self.space  = space
+        
         self.body = pymunk.Body()
         self.body.position = position
+        shape = pymunk.Circle(self.body,20)
+        shape.mass = mass 
+        shape.color = None#(255,255,255,0)
+        shape.filter = pymunk.ShapeFilter(categories=Categories.LANDER_CAT,mask=Categories.TERRAIN_CAT)
+        space.add(self.body,shape)
+        
+    def draw(self):
+        pygame.draw.circle(self.screen,(255,0,0),self.body.position,radius=20)
+        
+    def kill(self):
+        for shape in self.body.shapes:
+            self.space.remove(shape)
+        self.space.remove(self.body)
+
+class LanderFactory:
+    def __init__(self,screen,space,num_landers=50):
+        self.screen = screen
+        self.space  = space
+        
+        self.num_landers = num_landers
+        self.landers = []
     
-    def draw():
+    def create_landers(self):
+        for i in range(self.num_landers):
+            x = random.randint(10,self.screen.get_size()[0])
+            self.landers.append(Lander((x,100),self.screen,self.space,100,i))
+            
+    def draw_landers(self):
+        for lander in self.landers:
+            lander.draw()
+
+class Simulation:
+    def __init__(self,
+                 width=1280,
+                 height=720,
+                 terrain_corners = 500):
+        
+        pygame.init()
+        
+        self.width   = width
+        self.height  = height 
+        
+        self.screen  = pygame.display.set_mode((width, height))
+        self.clock   = pygame.time.Clock()
+        self.fps     = 60
+        self.dt      = 1/self.fps
+        self.running = True
+        
+        self.terrain_vertexes     = []
+        self.terrain_break_count  = terrain_corners
+        self.terrain_exaggeration = 300
+        
+        self.space         = pymunk.Space()
+        self.space.gravity = (0, 981)
+        
+        self.lander_factory = LanderFactory(self.screen,self.space)
+        
+        self.init()
+    
+    def init(self):    
+        self.generate_terrain_vertexes()
+        self.generate_terrain_physics()
+
+        self.lander_factory.create_landers()
+    
+    def loop(self):
+        self.draw_terrain()
+        self.lander_factory.draw_landers()
+    
+    def end(self):
         pass
+    
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    
+            self.screen.fill("black")
+            
+            self.loop()
+
+            pygame.display.flip()
+            
+            self.space.step(self.dt)        
+            self.clock.tick(self.fps)
+
+        self.end()
+        pygame.quit()
+        
+    def generate_terrain_vertexes(self):
+        terrain_break_heights = [generate_noise([x/self.terrain_break_count,0]) for x in range(self.terrain_break_count)]
+        
+        start_index = 0
+        gap = self.width / (self.terrain_break_count - 1) 
+        
+        self.terrain_vertexes.append([start_index,self.height])
+        
+        for index, height in enumerate(terrain_break_heights[:-1]):
+            height1 = min(self.height-10,(self.height * 0.8) + (height * self.terrain_exaggeration))
+            p1 = [start_index, height1]
+
+            start_index += gap
+
+            height2 = min(self.height-10,(self.height * 0.8) + (terrain_break_heights[index + 1] * self.terrain_exaggeration))
+            p2 = [start_index, height2]
+            
+            self.terrain_vertexes.append(p1)
+            self.terrain_vertexes.append(p2)
+        
+        if len(terrain_break_heights) > 1:
+            final_height = min(self.height-10,(self.height * 0.8) + (terrain_break_heights[-1] * self.terrain_exaggeration))
+            self.terrain_vertexes.append([self.width,final_height])
+            
+        self.terrain_vertexes.append([self.width,self.height])
+        
+    def generate_terrain_physics(self):
+        terrain_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.space.add(terrain_body)
+        
+        for a,b in pairwise(self.terrain_vertexes[1:-1]):
+            v1 = a
+            v2 = b
+            v3 = [ b[0] , self.height ]
+            v4 = [ a[0] , self.height ]
+            
+            shape = pymunk.Poly(terrain_body, [v1,v2,v3,v4])
+            shape.filter = pymunk.ShapeFilter(categories=Categories.TERRAIN_CAT,mask=Categories.LANDER_CAT)
+            self.space.add(shape)
+        
+    def draw_terrain(self):
+        if not self.terrain_vertexes:
+            return
+        
+        pygame.draw.polygon(self.screen, (255, 255, 255), self.terrain_vertexes)
+    
--- 2D/utils.py
+++ 2D/utils.py
@@ -10,3 +10,10 @@ def generate_noise(coord):
     noise_val += 0.1 * noise3(coord)
     
     return noise_val
+
+
+
+def pairwise(iterable):
+    "s -> (s0, s1), (s2, s3), (s4, s5), ..."
+    a = iter(iterable)
+    return zip(a, a)
