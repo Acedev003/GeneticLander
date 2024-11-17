626ff96b38572949f566b0336084d076410c76c5
Acedev003 <acedeveloper003@gmail.com>
2024-11-17 09:40:49 +0530

    Add lander

--- lander.lua
+++ lander.lua
@@ -1,10 +1,30 @@
 Lander = Object.extend(Object)
 
-function Lander.new(self)
+function Lander.new(self,world,category,spawn_x,spawn_y)
+    self.body    = love.physics.newBody(world, spawn_x, spawn_y, 'dynamic')
+    self.shape   = love.physics.newRectangleShape(50, 50)
+    self.fixture = love.physics.newFixture(self.body, self.shape)
+
+    self.image   = love.graphics.newImage("assets/Lander.png")
+    
+    self.fixture:setGroupIndex( -10 )
+    self.body:setMass(10000)
+
+    self.alive = true
+end
+
+function Lander.update(self)
+    
 end
 
 function Lander.draw(self)
-end
 
-function Lander.draw(self)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(
+        self.image,
+        self.body:getX(),
+        self.body:getY(),
+        self.body:getAngle(),
+        1,1,50/2,50/2
+    ) 
 end
